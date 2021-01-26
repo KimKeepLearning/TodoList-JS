7f3834e20ba7f395d48a3ffd2ecfa1fe6bf8da37
Y. Huang <18261195537@163.com>
2021-01-26 13:02:50 +0800

    UI finished

--- js/base.js
+++ js/base.js
@@ -1,3 +1,15 @@
 ;(function () {
     'use strict';
+
+    var $form_add_task = $('.add-task');
+    var new_task = {};
+
+    console.log("1");
+    $form_add_task.on("submit", function (e) {
+        new_task.content = $(this).find('input[name=content]').val();
+        console.log("2");
+        console.log(new_task.content);
+        e.preventDefault();
+    });
+
 })();
