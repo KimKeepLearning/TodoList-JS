b7552ac50a6818bf6bc8ec3b3fdfcae4dd95cc7c
Y. Huang <18261195537@163.com>
2021-01-26 21:49:39 +0800

    add delete function

--- js/base.js
+++ js/base.js
@@ -1,8 +1,9 @@
 ;(function () {
     'use strict';
 
-    var $form_add_task = $('.add-task'), $add_button = $('.add-task .submit');
-    // var new_task = {};
+    var $form_add_task = $('.add-task'),
+        $add_button = $('.add-task .submit'),
+        $delete_task;
     var task_list =[];
     
 
@@ -15,42 +16,64 @@
         if (!new_task.content) return;
         
         if (addTask(new_task)) {
-            render_task_list();
             $input.val('');
         }
 
 
     });
+    function listenTaskDelete() {
+        $delete_task.on("click", function (e) {
+            var $this = $(this);
+            var $item = $this.parent().parent();
+            var index = $item.data('index');
+            var deleteComfirm = confirm("确定删除？");
+            deleteComfirm ? deleteTask(index) : null;
+        });    
+    }
+    
 
     function addTask(new_task) {
         task_list.push(new_task);
-        store.set('task_list', task_list);
-        // store.clearAll();
+        refreshTaskList();
         return true;
     }
 
+    function refreshTaskList() {
+        store.set("task_list", task_list);
+        render_task_list();
+    }
+
+    function deleteTask(index) {
+        if (index === undefined || !task_list[index]) return;
+        delete task_list[index];
+        refreshTaskList();
+
+    }
     function init() {
-        // store.clearAll();
         task_list = store.get('task_list') || [];
         if (task_list.length) {
             render_task_list();
         }
+        
     }
     function render_task_list() {
         var $task_list = $('.task-list');
         $task_list.html('');
         for (let i = 0; i < task_list.length; i++){
-            var $task = render_task_tpl(task_list[i]);
+            var $task = render_task_tpl(task_list[i], i);
             $task_list.append($task);
         }
+        $delete_task = $(".action.delete");
+        listenTaskDelete();
     }
-    function render_task_tpl(data) {
+    function render_task_tpl(data, index) {
+        if (!data || !index) return;
         var list_item_tpl =
-            '<div class="task-item">' +
+            '<div class="task-item" data-index="'+ index+'">' +
             '<span><input type="checkbox"></span>' +
             '<span class="task-content">' + data.content + '</span>' +
             '<span class="fr">' +
-                '<span class="action"> 删除</span>' +
+                '<span class="action delete"> 删除</span>' +
                 '<span class="action"> 详情</span>' +
             '</span>' +
             '</div>';
